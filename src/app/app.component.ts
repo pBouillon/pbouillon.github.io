c0eb87c759bb554fbedc9e28009e27bf2af843a2
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-06 21:30:04 +0200

    Create the app home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
+import { NavbarLink } from './core/models/navbar-link.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Pierre Bouillon';
+
+  links: NavbarLink[] = [
+    {
+      href: '',
+      label: 'Accueil',
+      isExternal: false,
+    },
+    {
+      href: 'https://linkedin.com/in/pierre-bouillon',
+      label: 'LinkedIn',
+      isExternal: true,
+    },
+    {
+      href: 'https://github.com/pbouillon',
+      label: 'GitHub',
+      isExternal: true,
+    },
+  ];
+
+  showMobileNavbar = false;
+}
