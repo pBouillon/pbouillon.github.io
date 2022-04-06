d485b06f48645958f9415aaf9fd6ac5b5d594020
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-06 18:47:46 +0200

    Remove the custom display for the navbar-link

--- src/app/shared/atoms/navbar-link/navbar-link.stories.ts
+++ src/app/shared/atoms/navbar-link/navbar-link.stories.ts
@@ -1,7 +1,6 @@
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import {
-  componentWrapperDecorator,
   Meta,
   moduleMetadata,
   Story,
@@ -16,12 +15,6 @@ export default {
       imports: [CommonModule, RouterModule.forRoot([])],
       providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     }),
-    componentWrapperDecorator(
-      (story) => `
-      <div style="width: fit-content">
-        ${story}
-      </div>`
-    ),
   ],
   title: 'Atoms/Navbar link',
 } as Meta;
