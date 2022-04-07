d0f3c860f08ae3a725fc5601d4bb1d07d1b4c003
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-07 07:54:33 +0200

    Add tests for the dark mode toggler

--- src/app/shared/atoms/dark-mode-toggler-selector/dark-mode-toggler-selector.component.spec.ts
+++ src/app/shared/atoms/dark-mode-toggler-selector/dark-mode-toggler-selector.component.spec.ts
@@ -4,6 +4,8 @@ import { DarkModeTogglerSelectorComponent } from './dark-mode-toggler-selector.c
 describe('DarkModeTogglerSelectorComponent', () => {
   let component: DarkModeTogglerSelectorComponent;
   let fixture: ComponentFixture<DarkModeTogglerSelectorComponent>;
+  let selector: HTMLSelectElement;
+  let span: HTMLSpanElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +16,34 @@ describe('DarkModeTogglerSelectorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DarkModeTogglerSelectorComponent);
     component = fixture.componentInstance;
+
+    selector = fixture.nativeElement.querySelector('select');
+    span = fixture.nativeElement.querySelector('span');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the moon icon when not in dark mode', () => {
+    component.isDarkModeEnabled = false;
+    fixture.detectChanges();
+
+    const icon = span.querySelectorAll('i')[0];
+
+    expect(selector.value).toBe('light');
+    expect(span.textContent).toContain('Clair');
+    expect(icon.classList.contains('bi-sun')).toBeTruthy();
+  });
+
+  it('should display the sun icon when in dark mode', () => {
+    component.isDarkModeEnabled = true;
+    fixture.detectChanges();
+
+    const icon = span.querySelectorAll('i')[0];
+
+    expect(selector.value).toBe('dark');
+    expect(span.textContent).toContain('Sombre');
+    expect(icon.classList.contains('bi-moon-stars')).toBeTruthy();
   });
 });
--- src/app/shared/molecules/navbar/navbar.component.spec.ts
+++ src/app/shared/molecules/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DarkModeTogglerSelectorComponent } from '../../atoms/dark-mode-toggler-selector/dark-mode-toggler-selector.component';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -12,7 +13,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [NavbarComponent],
+      declarations: [NavbarComponent, DarkModeTogglerSelectorComponent],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
