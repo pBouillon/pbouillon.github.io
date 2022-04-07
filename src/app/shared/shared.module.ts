cff90bfadd70ea35c25292682297f4bd13d4ca79
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-07 21:20:19 +0200

    Format the shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarLinkComponent } from './atoms/navbar-link/navbar-link.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './molecules/navbar/navbar.component';
-import { NavbarBlockComponent } from './molecules/navbar-block/navbar-block.component';
 import { DarkModeTogglerIconComponent } from './atoms/dark-mode-toggler-icon/dark-mode-toggler-icon.component';
 import { DarkModeTogglerSelectorComponent } from './atoms/dark-mode-toggler-selector/dark-mode-toggler-selector.component';
+import { NavbarLinkComponent } from './atoms/navbar-link/navbar-link.component';
 import { PageTitleComponent } from './atoms/page-title/page-title.component';
-
-
+import { NavbarBlockComponent } from './molecules/navbar-block/navbar-block.component';
+import { NavbarComponent } from './molecules/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -19,14 +17,7 @@ import { PageTitleComponent } from './atoms/page-title/page-title.component';
     DarkModeTogglerSelectorComponent,
     PageTitleComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-  ],
-  exports: [
-    NavbarComponent,
-    NavbarBlockComponent,
-    PageTitleComponent,
-  ],
+  imports: [CommonModule, RouterModule],
+  exports: [NavbarComponent, NavbarBlockComponent, PageTitleComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
