3c0be147c56541645303ede4f7b7ae1ebc0caa01
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-16 09:05:45 +0200

    Cleanup the about-me component

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styles: [
-  ]
+  styles: ['p { @apply md:text-justify md:text-lg }'],
 })
-export class AboutMeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AboutMeComponent {}
