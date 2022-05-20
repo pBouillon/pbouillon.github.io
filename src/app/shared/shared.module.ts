eb93df014b8a381266703725e3522e84508d5a19
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-05-20 18:23:43 +0200

    Move the core components from shared to core

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,23 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { DarkModeTogglerIconComponent } from './navbar/dark-mode-toggler-icon/dark-mode-toggler-icon.component';
-import { DarkModeTogglerSelectorComponent } from './navbar-block/dark-mode-toggler-selector/dark-mode-toggler-selector.component';
-import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
-import { TagComponent } from './tag/tag.component';
 import { CardPlaceholderComponent } from './card-placeholder/card-placeholder.component';
 import { CardComponent } from './card/card.component';
-import { NavbarBlockComponent } from './navbar-block/navbar-block.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { TagComponent } from './tag/tag.component';
 
 @NgModule({
   declarations: [
     CardComponent,
     CardPlaceholderComponent,
-    DarkModeTogglerIconComponent,
-    DarkModeTogglerSelectorComponent,
-    NavbarBlockComponent,
-    NavbarComponent,
-    NavbarLinkComponent,
     TagComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
   exports: [
     CardComponent,
     CardPlaceholderComponent,
-    NavbarBlockComponent,
-    NavbarComponent,
   ],
 })
 export class SharedModule {}
