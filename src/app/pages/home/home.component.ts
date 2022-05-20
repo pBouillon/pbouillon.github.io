7a3b9e1b91891c7d604d218ffe4dd05db60d5adc
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-05-20 17:36:12 +0200

    Center the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styles: [':host { @apply flex-grow flex; }'],
 })
 export class HomeComponent {}
