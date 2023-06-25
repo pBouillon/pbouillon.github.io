e4e8c90e4a460239765b14aeb92c7e8657459d99
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-06-25 11:47:37 +0200

    Add a navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,42 @@
+<div class="drawer drawer-end">
+	<input id="navbar-drawer" type="checkbox" class="drawer-toggle" />
+
+	<!-- Navbar -->
+	<div class="drawer-content flex flex-col">
+		<div class="navbar bg-base-100 shadow">
+			<div class="flex-1">
+				<a href="/" class="btn-ghost btn text-2xl normal-case">Pierre Bouillon</a>
+			</div>
+
+			<!-- Sidebar Toggle -->
+			<div class="flex-none lg:hidden">
+				<label for="navbar-drawer" class="btn-ghost btn-square btn">
+					<i class="bi bi-list text-2xl" />
+				</label>
+			</div>
+
+			<!-- Navigation Items -->
+			<nav class="hidden flex-none lg:block">
+				<ul class="menu menu-horizontal text-xl">
+					<li>
+						<a href="https://github.com/pbouillon" target="_blank"
+							>GitHub <i class="bi bi-box-arrow-up-right" /></a
+						>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+	<!-- Sidebar Content -->
+	<nav class="drawer-side">
+		<label for="navbar-drawer" class="drawer-overlay" />
+		<ul class="menu h-full w-80 bg-base-200 p-4 text-xl">
+			<li>
+				<a href="https://github.com/pbouillon" target="_blank"
+					>GitHub <i class="bi bi-box-arrow-up-right" /></a
+				>
+			</li>
+		</ul>
+	</nav>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+
+	import Navbar from '$lib/components/navbar.svelte';
 </script>
+
+<Navbar />
 
 <slot />
