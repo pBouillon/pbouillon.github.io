b85c2af508567f5f277431326d130a3bd4eaca7b
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-08-26 22:19:17 +0200

    Load preferred language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,25 @@
+import { browser } from '$app/environment';
+import { loadTranslations } from '$lib/translations';
+import type { LayoutLoad } from './$types';
+
 // Configure SSR - https://kit.svelte.dev/docs/adapter-static#usage
 export const prerender = true;
+
+export const load: LayoutLoad = async ({ url }) => {
+  const { pathname } = url;
+  const initLocale = getInitialLocale();
+
+  await loadTranslations(initLocale, pathname);
+
+  return { locale: initLocale, route: pathname };
+};
+
+function getInitialLocale(): string {
+  if (!browser) return 'en';
+
+  try {
+    return window.navigator.language.split('-')[0];
+  } catch (e) {
+    return 'en';
+  }
+}
