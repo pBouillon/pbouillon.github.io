c807d7bf1eb1a619d998763b8386706fdc682c24
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-10-03 17:47:12 +0200

    Fix z-index

--- src/routes/blog/dev-article.svelte
+++ src/routes/blog/dev-article.svelte
@@ -6,9 +6,7 @@
   export let article: Article;
 </script>
 
-<article
-  class="card -z-10 my-5 w-96 rounded-lg border border-primary shadow-xl"
->
+<article class="card my-5 w-96 rounded-lg border border-primary shadow-xl">
   <figure>
     <LazyLoadedImage src={article.coverImage} alt={article.title} />
   </figure>
