c586ce7e0c3dac393ca5f3a5fd217ff39aa351a2
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-18 14:51:29 +0200

    chore: move the footer to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from '$app/state';
+  import BrutFooter from '$lib/components/BrutFooter.svelte';
   import BrutNav from '$lib/components/BrutNav.svelte';
   import { m } from '$lib/paraglide/messages';
   import { locales, localizeHref } from '$lib/paraglide/runtime';
@@ -15,6 +16,8 @@
 <BrutNav />
 
 {@render children?.()}
+
+<BrutFooter />
 
 <!-- https://inlang.com/m/gerre34r/library-inlang-paraglideJs/sveltekit#static-site-generation-ssg -->
 <div class="hidden">
