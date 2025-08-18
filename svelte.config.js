156ff6c1830608bfc51babab67b27e236fa53ae9
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-18 07:57:38 +0200

    fix: yet another attempt at avoiding redirection for "fr/"

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,13 @@ import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: [vitePreprocess(), mdsvex()],
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+  },
   extensions: ['.svelte', '.svx'],
   compilerOptions: { runes: true },
-  trailingSlash: 'always',
+  trailingSlash: 'never',
 };
 
 export default config;
